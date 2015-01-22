edf4228e4e705397e4c2911787ea8869fa361d00
Harold Pimentel <haroldpimentel@gmail.com>
2015-01-22 14:04:43 -0800

    wip

--- sims.R
+++ sims.R
@@ -1,3 +1,5 @@
+library("scales")
+
 options(cores = 43)
 options(mc.cores = 43)
 
@@ -318,3 +320,269 @@ save(
     sim_c_res, c_counts,
     sim_b_res, b_counts,
     file = "sim_res.RData")
+
+
+
+load("~/Dropbox/blog/exploring_k/results/sim_res.RData", verbose = TRUE)
+
+# add sample size to each set of results
+add_column_res <- function(res_list, counts_list)
+{
+
+    lapply(seq_along(res_list), function(i)
+        {
+            res <- res_list[[i]]
+            lapply(res, function(sol){
+                mutate(sol, sample_size = counts_list[[i]])
+        })
+        })
+}
+
+join_groups <- function(res_list, sol_names)
+{
+    res <- list()
+    for (i in seq_along(sol_names))
+    {
+        tmp <- lapply(res_list, function(x) x[[sol_names[i]]])
+        res[[sol_names[i]]] <- rbind_all(tmp)
+    }
+
+    res
+}
+
+method_names <- c("gap", "pham", "pred_strength")
+
+sim_b_joined <- join_groups(add_column_res(sim_b_res,
+        lapply(b_counts, paste, sep = "", collapse = ", ")),
+    method_names)
+sim_c_joined <- join_groups(add_column_res(sim_c_res,
+        lapply(c_counts, paste, sep = "", collapse = ", ")),
+    method_names)
+sim_d_joined <- join_groups(add_column_res(sim_d_res,
+        lapply(d_counts, paste, sep = "", collapse = ", ")),
+    method_names)
+sim_e_joined <- join_groups(add_column_res(sim_e_res, e_counts),
+    method_names)
+
+
+get_k_opt <- function(data_joined)
+{
+    rbind_all(lapply(names(data_joined),
+            function(x)
+            {
+                sub_list <- data_joined[[x]]
+                select(sub_list, sim_num, k_opt, sample_size) %>%
+                    mutate(method = x) %>%
+                    distinct()
+            })) %>%
+        mutate(sample_size = factor(sample_size, levels = unique(sample_size)))
+}
+
+sim_b_k_opt <- get_k_opt(sim_b_joined)
+sim_c_k_opt <- get_k_opt(sim_c_joined)
+sim_d_k_opt <- get_k_opt(sim_d_joined)
+sim_e_k_opt <- get_k_opt(sim_e_joined)
+
+b_plot <- ggplot(sim_b_k_opt, aes(k_opt, fill = method), group = method) +
+    geom_histogram(position = "dodge") +
+    facet_wrap(~ sample_size) +
+    theme_bw() +
+    xlab("k selected") +
+    ylim(0, 100)
+
+c_plot <- ggplot(sim_c_k_opt, aes(k_opt, fill = method), group = method) +
+    geom_histogram(position = "dodge") +
+    facet_wrap(~ sample_size) +
+    theme_bw() +
+    xlab("k selected") +
+    ylim(0, 100)
+
+d_plot <- ggplot(sim_d_k_opt, aes(k_opt, fill = method), group = method) +
+    geom_histogram(position = "dodge") +
+    facet_wrap(~ sample_size) +
+    theme_bw() +
+    xlab("k selected") +
+    ylim(0, 100)
+
+e_plot <- ggplot(sim_e_k_opt %>% filter(sample_size != "15000"),
+        aes(k_opt, fill = method), group = method) +
+    geom_histogram(position = "dodge") +
+    facet_wrap(~ sample_size) +
+    theme_bw() +
+    xlab("k selected") +
+    ylim(0, 100)
+
+ggplot(sim_e_k_opt,
+        aes(k_opt, fill = method), group = method) +
+    geom_histogram(position = "dodge") +
+    facet_wrap(~ sample_size) +
+    theme_bw() +
+    xlab("k selected") +
+    ylim(0, 100)
+
+library("gridExtra")
+
+grid.arrange(
+    b_plot + theme(legend.position = c(0.90, 0.77)) + ggtitle("(b) truth: k = 3") + scale_x_continuous(limits = c(1, 10), breaks = 1:10),
+    c_plot + theme(legend.position="none") + ggtitle("(c) truth: k = 4") +
+    scale_x_continuous(limits = c(1, 10),
+        breaks = (1:10),
+        labels = as.character(1:10)),
+    d_plot + theme(legend.position="none") + ggtitle("(d) truth: k = 4") +  scale_x_continuous(limits = c(1, 10),
+        breaks = (1:10),
+        labels = as.character(1:10)),
+    e_plot + theme(legend.position="none") + ggtitle("(e) truth: k = 2") + scale_x_continuous(limits = c(1, 10),
+        breaks = (1:10),
+        labels = as.character(1:10)))
+dev.print(pdf, "~/Dropbox/blog/exploring_k/part2/img/optimal_k.pdf")
+dev.off()
+
+
+b_table <- sim_b_k_opt %>%
+    group_by(method, sample_size, k_opt) %>%
+    summarise(n = n()) %>%
+    ungroup() %>%
+    arrange(sample_size, method, k_opt)
+
+################################################################################
+# look at timings
+################################################################################
+
+
+get_timings <- function(data_joined)
+{
+    rbind_all(lapply(names(data_joined),
+        function(x)
+        {
+            sub_list <- data_joined[[x]]
+            select(sub_list, sim_num, elapsed, sample_size) %>%
+                mutate(method = x) %>%
+                distinct()
+        })) %>%
+        mutate(sample_size = factor(sample_size, levels = unique(sample_size)))
+}
+
+time_plot <- function(time_data)
+{
+    ggplot(time_data, aes(elapsed, fill = method), group = method) +
+        geom_histogram(position = "stack", binwidth = 0.1/2) +
+        facet_wrap(~ sample_size) +
+        theme_bw() +
+        xlab("Elapsed time (s)") +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1))
+}
+
+time_plot2 <- function(time_data)
+{
+    time_plot(time_data) +
+        theme(legend.position="none") +
+        scale_x_continuous(
+            limits = c(2^-5, 2^15),
+            trans = log2_trans(),
+            breaks = trans_breaks("log2", function(x) 2^x),
+            labels = trans_format("log2", math_format(2^.x))) +
+        ylim(0, 50)
+}
+
+sim_b_time <- get_timings(sim_b_joined)
+sim_c_time <- get_timings(sim_c_joined)
+sim_d_time <- get_timings(sim_d_joined)
+sim_e_time <- get_timings(sim_e_joined) %>%
+    filter(sample_size != "15000")
+
+b_time_plot <- ggplot(sim_b_time, aes(elapsed, fill = method), group = method) +
+    geom_histogram(position = "stack", binwidth = 3.5) +
+    facet_wrap(~ sample_size) +
+    theme_bw() +
+    xlab("Elapsed time (s)") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+b_time_plot <- time_plot(sim_b_time) + theme(legend.position="none")
+c_time_plot <- time_plot(sim_c_time) + theme(legend.position="none")
+d_time_plot <- time_plot(sim_d_time) + theme(legend.position="none")
+e_time_plot <- time_plot(sim_e_time) + theme(legend.position="none")
+
+b_time_plot + theme(legend.position = c(0.90, 0.77))
+time_plot(sim_d_time) + theme(legend.position="none") + scale_x_log10()
+
+time_plot(sim_b_time) +
+    theme(legend.position="none") +
+    scale_x_continuous(
+        limits = c(2^-5, 2^15),
+        trans = log2_trans(),
+        breaks = trans_breaks("log2", function(x) 2^x),
+        labels = trans_format("log2", math_format(2^.x)))
+
+
+time_plot2(sim_b_time)
+time_plot2(sim_d_time)
+
+grid.arrange(time_plot2(sim_b_time) +
+            ggtitle("(b) truth: k = 3") +
+            theme(legend.position = c(0.90, 0.77)),
+        time_plot2(sim_c_time) + ggtitle("(c) truth: k = 4"),
+        time_plot2(sim_d_time) + ggtitle("(d) truth: k = 4"),
+        time_plot2(sim_e_time) + ggtitle("(e) truth: k = 2"))
+
+ggplot(sim_b_time, aes(method, elapsed, colour = method), colour = method) +
+    geom_boxplot() +
+    facet_wrap(~ sample_size) +
+    theme_bw() +
+    xlab("Elapsed time (s)") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggplot(sim_b_time, aes(elapsed, colour = method)) +
+    geom_density() +
+    facet_wrap(~ sample_size) +
+    theme_bw() +
+    xlab("Elapsed time (s)") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+install.packages("rgl")
+library("rgl")
+
+set.seed(99)
+d <- sim_e(7500)$data
+d_res <- prediction.strength(d,
+    Gmax = 10,
+    runs = 50,
+    iter.max = 1000)
+
+set.seed(30)
+d_km <- kmeans(d, 10, iter.max = 1000, nstart = 50)
+d_w_clust <- cbind(d, d_km$cluster)
+
+save(d, d_res, d_km, file = "~/Dropbox/blog/exploring_k/part2/results/d.RData")
+
+colnames(d) <- c("x", "y", "z")
+
+plot3d(d, col = d_km$cluster)
+rgl.postscript("~/Dropbox/blog/exploring_k/part2/img/elongated_7500.pdf", "pdf")
+
+
+data <- sim_e(100)$data
+plot3d(data)
+
+
+b_plot <- ggplot(sim_b_k_opt, aes(k_opt, fill = method), group = method) +
+    geom_histogram(position = "dodge") +
+    facet_wrap(~ sample_size) +
+    theme_bw() +
+    xlab("k selected") +
+    ylim(0, 100)
+
+as.data.frame(sim_b_k_opt, stringsAsFactors = FALSE) %>%
+    ggvis(~k_opt) %>%
+    layer_histograms()
+
+hi <- sim_b()$data
+hi <- as.data.frame(hi)
+colnames(hi) <- c("dx", "dy")
+
+hi <- hi %>%
+    mutate(info = letters[sample.int(length(letters),
+            nrow(hi), replace = TRUE)])
+
+hi %>%
+    ggvis(~dx, ~dy, key := ~info) %>%
+    add_tooltip(function(el) {el$info}, "hover")
